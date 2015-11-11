a63a1078b27c5541f14521372a2b6d4a17a3bc1f
Richard <richarddeng88@gmail.com>
2015-11-11 16:58:04 -0500

    random forest

--- project do on college 11 11.R
+++ project do on college 11 11.R
@@ -1,31 +1,51 @@
+##DATA READING
 train_pml <- read.csv("data/practical_machine_l/pml-training.csv",stringsAsFactors = T)
 test_pml <- read.csv("data/practical_machine_l/pml-testing.csv",stringsAsFactors = T)
 
-# DEAL WITH NA
-na <- sapply(test_pml, function(x){sum(is.na(x))})
-train_na <- subset(train_pml, select = !(names(train_pml) %in% names(na[na==20])))
-test_na <- subset(test_pml, select = !(names(test_pml) %in% names(na[na==20])))
 
+## PREPROCESSING
 
-# TRAIN DATA SPLITTING FOR MODEL TESTING
-train_na <- train_na[,c(-1,-2)]
-library(caret); set.seed(1001)
-intrain <- createDataPartition(y=train_na$classe, p=0.8, list = F)
-training <- train_na[intrain,]
-testing <- train_na[-intrain,]
+    # DATA SPLITING
+    library(caret); set.seed(1001)
+    intrain <- createDataPartition(y=train_pml$classe, p=0.8, list = F)
+    training <- train_pml[intrain,]
+    validation <- train_pml[-intrain,]
 
+    # FEATURE SELECTION
+        # DEAL COMLUMNS WITH NAs
+        na <- sapply(test_pml, function(x){sum(is.na(x))})
+        training <- subset(training, select = !(names(training) %in% names(na[na==20])))
+        #test_na <- subset(test_pml, select = !(names(test_pml) %in% names(na[na==20])))
+    
+        # REMOVE ZERO COVARIATE
+        nsv <- nearZeroVar(training) # optiong: , saveMetrics = T.
+                                    # nearZeroVar diagnoses predictors that has one unique value
+                                    # or that have very few unique values. 
+        training <- training[,-nsv]
 
-## UNIQUENESS CHECK - REMOVE ZERO COVARIATE
-length <- sapply(training, function(x){length(unique(x))})
-length[length==2]  # there is no clumn with only one value
-nsv <- nearZeroVar(training, saveMetrics = T) # nearZeroVar diagnoses predictors that has one unique value
-                                        # or that have very few unique values. 
-
-
-## CORRELATION CHECK 
+        # REMOVE DESCREIPTIVE FEATURES. 
+        excludecols <- c("X", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", 
+                          "cvtd_timestamp", "num_window")
+        training <- training[, !names(training) %in% excludecols]
+        
+## CORRELATION CHECK, #####I DON'T KNOW WHAT I SHOULD DO.#####
 M <- abs(cor(training[,c(-3,-4,-58)])) # leave out the charater type
 diag(M) <- 0 # every variable has correlation 1 with itself. So i don't need to care the diag(M)
 which(M>0.8, arr.ind = T)
+
+## MODEL TRAIN - WE WILL USE random forest as our model IMPLEMENTED IN THE randomForest PACKAGE
+library(randomForest)
+rfModel <- randomForest(classe~., data=training, importance= T, ntrees=10)
+
+## MODEL VALIDATION - VALIDATION SET ACCURACY
+pre_validation <- predict(rfModel,validation)
+print(confusionMatrix(pre_validation, validation$classe))
+
+
+
+
+
+
 
 
 
