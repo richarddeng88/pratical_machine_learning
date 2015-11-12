ee998652335ee008a7d5db78687cefb9f72aaf9f
Richard <richarddeng88@gmail.com>
2015-11-12 08:29:50 -0500

    11 12

--- project - 2nd KNN method 11 11.R
+++ project - 2nd KNN method 11 11.R
@@ -6,36 +6,36 @@ test_pml <- read.csv("data/practical_machine_l/pml-testing.csv",stringsAsFactors
 
 ## PREPROCESSING
 
-# FEATURE SELECTION
-# DEAL COMLUMNS WITH NAs
-na <- sapply(test_pml, function(x){sum(is.na(x))})
-training_knn <- subset(train_pml, select = !(names(train_pml) %in% names(na[na==20])))
-testing_knn <- subset(test_pml, select = !(names(test_pml) %in% names(na[na==20])))
-
-# REMOVE DESCREIPTIVE FEATURES. 
-excludecols <- c("X", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", 
-                 "cvtd_timestamp", "num_window", "new_window")
-training_knn <- training_knn[, !names(training_knn) %in% excludecols]
-testing_knn <- testing_knn[, !names(testing_knn) %in% excludecols]
-
-
-# REMOVE ZERO COVARIATE # i don't use nearZeroVar() because it's hard to do the same on test dataset
-length <- sapply(training_knn, function(x){length(unique(x))})
-length[length<3]
-
-# NORMALIZE DATA both training_knn and testing_knn
-normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x)))}
-tem <- rbind(training_knn[,-53], testing_knn[-53])
-t_n <- as.data.frame(lapply(tem, normalize))
-training_knn<- cbind(t_n[-20:-1,], training_knn[53])
-testing_knn <- t_n[19623:19642,]
-# DATA SPLITING
-library(caret); set.seed(1001)
-intrain <- createDataPartition(y=training_knn$classe, p=0.8, list = F)
-training_body <- training_knn[intrain,][,-53]
-validation_body <- training_knn[-intrain,][,-53]
-training_label <- training_knn[intrain,][,53]
-validation_label <- training_knn[-intrain,][,53]
+    # FEATURE SELECTION
+        # DEAL COMLUMNS WITH NAs
+        na <- sapply(test_pml, function(x){sum(is.na(x))})
+        training_knn <- subset(train_pml, select = !(names(train_pml) %in% names(na[na==20])))
+        testing_knn <- subset(test_pml, select = !(names(test_pml) %in% names(na[na==20])))
+        
+        # REMOVE DESCREIPTIVE FEATURES. 
+        excludecols <- c("X", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2", 
+                         "cvtd_timestamp", "num_window", "new_window")
+        training_knn <- training_knn[, !names(training_knn) %in% excludecols]
+        testing_knn <- testing_knn[, !names(testing_knn) %in% excludecols]
+        
+        
+        # REMOVE ZERO COVARIATE # i don't use nearZeroVar() because it's hard to do the same on test dataset
+        length <- sapply(training_knn, function(x){length(unique(x))})
+        length[length<3]
+        
+        # NORMALIZE DATA both training_knn and testing_knn
+        normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x)))}
+        tem <- rbind(training_knn[,-53], testing_knn[-53])
+        t_n <- as.data.frame(lapply(tem, normalize))
+        training_knn<- cbind(t_n[-20:-1,], training_knn[53])
+        testing_knn <- t_n[19623:19642,]
+        # DATA SPLITING
+        library(caret); set.seed(1001)
+        intrain <- createDataPartition(y=training_knn$classe, p=0.8, list = F)
+        training_body <- training_knn[intrain,][,-53]
+        validation_body <- training_knn[-intrain,][,-53]
+        training_label <- training_knn[intrain,][,53]
+        validation_label <- training_knn[-intrain,][,53]
 
 ## CORRELATION CHECK, #####I DON'T KNOW WHAT I SHOULD DO.#####
 M <- abs(cor(training[,c(-3,-4,-58)])) # leave out the charater type
@@ -50,62 +50,62 @@ print(confusionMatrix(pred_vlidation, validation_label))
 #        after mormalization the accuracy has been increased to 0.7346 (k=100)
 
 ## PERFORMACE INCREASE
-
-# try different K - k=50
-pred_vlidation<- knn(training_body,validation_body, training_label, k=50 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-##   when change k =50, accuracy has improved to 0.7917
-
-# try different K - k=20    
-pred_vlidation<- knn(training_body,validation_body, training_label, k=20 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-##   when change k =50, accuracy has improved to 0.8766    
-
-# try different K - k=5 
-pred_vlidation<- knn(training_body,validation_body, training_label, k=5 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-##   when change k =5, accuracy has improved to 0.9618
-
-# try different K - k=2 
-pred_vlidation<- knn(training_body,validation_body, training_label, k=2 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-##   when change k =2, accuracy has improved to 0.9824  
-
-# try different K - k=1 
-pred_vlidation<- knn(training_body,validation_body, training_label, k=1 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-##   when change k =1, accuracy has improved to 0.9898  
+    
+    # try different K - k=50
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=50 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    ##   when change k =50, accuracy has improved to 0.7917
+    
+    # try different K - k=20    
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=20 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    ##   when change k =50, accuracy has improved to 0.8766    
+    
+    # try different K - k=5 
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=5 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    ##   when change k =5, accuracy has improved to 0.9618
+    
+    # try different K - k=2 
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=2 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    ##   when change k =2, accuracy has improved to 0.9824  
+    
+    # try different K - k=1 
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=1 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    ##   when change k =1, accuracy has improved to 0.9898  
 
 # PERFORMANCE INCREASE 2
-# USE Z-SCORE SDANDARDIZE DATA
-t_z <- as.data.frame(scale(training_knn[,-53]))
-training_knn_z<- cbind(t_z, training_knn[53])
-
-# DATA SPLITING
-library(caret); set.seed(1001)
-intrain <- createDataPartition(y=training_knn_z$classe, p=0.8, list = F)
-training_body <- training_knn_z[intrain,][,-53]
-validation_body <- training_knn_z[-intrain,][,-53]
-training_label <- training_knn_z[intrain,][,53]
-validation_label <- training_knn_z[-intrain,][,53]    
+    # USE Z-SCORE SDANDARDIZE DATA
+    t_z <- as.data.frame(scale(training_knn[,-53]))
+    training_knn_z<- cbind(t_z, training_knn[53])
+    
+    # DATA SPLITING
+    library(caret); set.seed(1001)
+    intrain <- createDataPartition(y=training_knn_z$classe, p=0.8, list = F)
+    training_body <- training_knn_z[intrain,][,-53]
+    validation_body <- training_knn_z[-intrain,][,-53]
+    training_label <- training_knn_z[intrain,][,53]
+    validation_label <- training_knn_z[-intrain,][,53]    
 
 # PREDICTING
-pred_vlidation<- knn(training_body,validation_body, training_label, k=50 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-# AFTER Z-SCORE standardization, accuracy imporoved to 0.8318 with k=50
-
-pred_vlidation<- knn(training_body,validation_body, training_label, k=5 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-# AFTER Z-SCORE standardization, accuracy imporoved to 0.9674 with k=5
-
-pred_vlidation<- knn(training_body,validation_body, training_label, k=2 )
-print(confusionMatrix(pred_vlidation , validation_label))  
-# AFTER Z-SCORE standardization, accuracy imporoved to 0.9819 with k=2
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=50 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    # AFTER Z-SCORE standardization, accuracy imporoved to 0.8318 with k=50
+    
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=5 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    # AFTER Z-SCORE standardization, accuracy imporoved to 0.9674 with k=5
+    
+    pred_vlidation<- knn(training_body,validation_body, training_label, k=2 )
+    print(confusionMatrix(pred_vlidation , validation_label))  
+    # AFTER Z-SCORE standardization, accuracy imporoved to 0.9819 with k=2
 
 ## TESTING DATA PREDICTION
 pred_test <- knn(training_knn[,-53], testing_knn, training_knn[,53],k=2)
 
-
+pred_test <- knn(training_body, testing_knn, training_label,k=5)
 ## OUTPUT THE RESULT ACCORDING TO THE INSTRUCTURE. 
 answers <- as.vector(pred_test)
 
